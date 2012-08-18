a3bb17b295965a3d08895816804138009036696d
Jonathan Lange <jml@mumak.net>
2012-08-18 13:20:26 +0100

    Actually figure out anagrams.

--- src/anagrm/core.clj
+++ src/anagrm/core.clj
@@ -1,11 +1,20 @@
 (ns anagrm.core
   (:gen-class))
 
+(use '[clojure.java.io :only (reader)])
+
+(def ^:const DICTIONARY-FILE "/usr/share/dict/words")
+
 (defn anagram? [x y]
   "Is x an anagram of y?"
   (= (sort x) (sort y)))
 
+(defn find-anagrams [base-word word-list]
+  (filter (partial anagram? base-word) word-list))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Print the anagrams for a given word"
+  [base-word & args]
+  (with-open [dict (reader DICTIONARY-FILE)]
+    (doseq [word (find-anagrams base-word (line-seq dict))]
+      (println word))))
