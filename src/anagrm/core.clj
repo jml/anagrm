ad3f8cc67cfb5183531b20348f7b1ca5314f7cd8
Jonathan Lange <jml@mumak.net>
2012-08-18 15:48:39 +0100

    Subanagram searching.

--- src/anagrm/core.clj
+++ src/anagrm/core.clj
@@ -9,12 +9,27 @@
   "Is x an anagram of y?"
   (= (sort x) (sort y)))
 
+(defn sub-frequency? [a b]
+  (every? #(>= (get a (key %) -1) (val %)) b))
+
+(defn sub-anagram? [base-word sub-word]
+  (sub-frequency? (frequencies base-word) (frequencies sub-word)))
+
 (defn find-anagrams [base-word word-list]
   (filter (partial anagram? base-word) word-list))
+
+(defn word-search [search?]
+  (with-open [dict (reader DICTIONARY-FILE)]
+    (doseq [word (filter search? (line-seq dict))]
+      (println word))))
+
+;; XXX - add something to make sure words are of a certain length
+;; XXX - add a thing for guaranteeing they contain a certain letter
+;; XXX - case insensitive matching
+;; XXX - it's a little slow
 
 (defn -main
   "Print the anagrams for a given word"
   [base-word & args]
-  (with-open [dict (reader DICTIONARY-FILE)]
-    (doseq [word (find-anagrams base-word (line-seq dict))]
-      (println word))))
+  (word-search (partial sub-anagram? base-word)))
+
--- test/anagrm/core_test.clj
+++ test/anagrm/core_test.clj
@@ -6,3 +6,28 @@
   (is (anagram? "jabberwocky" "jabberwocky"))
   (is (anagram? "jabberwocky" "ykcowrebbaj"))
   (is (not (anagram? "cabbage" "brillig"))))
+
+(deftest test-subanagram
+  (are [x y] (sub-anagram? x y)
+       "jabberwocky" "jabberwocky"
+       "jabberwocky" "jabber"
+       "specific"    "pie"
+       "specific"    "piscie"
+       ))
+
+(deftest test-subanagram-of-containing-word
+  (is (not (sub-anagram? "jabber" "jabberwocky"))))
+
+(deftest test-subfrequency
+  (are [x y] (sub-frequency? x y)
+       {:a 2 :b 3 :c 4} {:a 1 :b 1 :c 1}
+       {} {}
+       {:a 2 :b 3 :c 4} {:a 2 :b 3 :c 4}
+       {:a 2 :b 3 :c 4} {:a 2}
+       ))
+
+(deftest test-not-subfrequency
+  (are [x y] (not (sub-frequency? x y))
+       {:a 2 :b 3 :c 4} {:a 2 :b 5 :c 4}
+       {:a 2 :b 3 :c 4} {:a 2 :b 3 :c 4 :d 3}
+       {:a 2 :b 3 :c 4} {:a 2 :b 3 :d 3}))
